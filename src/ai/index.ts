04dee5956bc25a34b8bc58b3e32d0c8672e2bcef
Cetc2211 <mpceciliotopetecruz@gmail.com>
2025-10-26 05:00:02 -0700

    Update index.ts

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,7 +1,7 @@
 'use server';
 
-import { generateStudentFeedback } from "./flows/generate-student-feedback-flow";
-import { generateGroupReportAnalysis } from "./flows/generate-group-report-analysis-flow";
+import { generateStudentFeedback } from "./generate-student-feedback-flow";
+import { generateGroupReportAnalysis } from "./generate-group-report-analysis-flow";
 
 export {
     generateStudentFeedback,
