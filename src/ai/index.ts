7f5a1a0008776ca380cdaf2e1956353e7f2aa771
Cetc2211 <mpceciliotopetecruz@gmail.com>
2025-10-31 23:18:36 +0000

    restore: apply user snapshot

--- src/ai/index.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { generateStudentFeedback } from "./flows/generate-student-feedback-flow";
-import { generateGroupReportAnalysis } from "./flows/generate-group-report-analysis-flow";
-
-export {
-    generateStudentFeedback,
-    generateGroupReportAnalysis,
-};
