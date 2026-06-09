b8f0cf008c5cf9da5dcefa1bb6a3a51a51f99031
Cetc2211 <mpceciliotopetecruz@gmail.com>
2026-06-09 22:05:42 +0000

    funtion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 import { getStorage } from 'firebase-admin/storage';
 import { createHmac, timingSafeEqual } from 'crypto';
 import { HttpsError, onCall, onRequest } from 'firebase-functions/v2/https';
-import { logger } from 'firebase-functions/logger';
+import * as logger from 'firebase-functions/logger';
 import { defineSecret } from 'firebase-functions/params';
 
 if (admin.apps.length === 0) {
